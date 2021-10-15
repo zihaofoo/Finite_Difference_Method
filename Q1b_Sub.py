d72e799b8904375c5e4e2a036df7c54e1d377082
zihaofoo <zihaofoo@mit.edu>
2021-10-15 16:43:03 -0400

    10/15 Push 1 "Question 1b "

---
+++ Q1b_Sub.py
@@ -0,0 +1,12 @@
+# Massachusetts Institute of Technology
+# Department of Mechanical Engineering
+author = "Zi Hao Foo"
+date = "Oct 2021"
+
+import numpy as np
+import scipy as sp
+from scipy.sparse import linalg
+from scipy import sparse
+import matplotlib.pyplot as plt
+import pandas as pd
+
