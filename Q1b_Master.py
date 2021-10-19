d18d4e2abecd7bcb478844c55d8bf6879879634f
zihaofoo <zihaofoo@mit.edu>
2021-10-19 16:26:47 -0400

    10/19 Push 1

--- Q1b_Master.py
+++ Q1b_Master.py
@@ -10,17 +10,17 @@ import scipy.sparse.linalg as splinalg
 from scipy import sparse
 import matplotlib.pyplot as plt
 import pandas as pd
-from Q1b_Sub import Solver_Iter, Flux, I_down, Plotting
+from Q1b_Sub import I_up, Solver_Iter, Flux, I_up, I_down, Plotting, MultiGrid
 
 Source_block = np.array([1, 7, 14, 16])
 # N_x = np.array([7, 13, 19, 25])                    # Number of nodes
 # N_y = np.array([7, 13, 19, 25])                   # Number of nodes
-N_Iter = 5000
+N_Iter = 2
 N_sol = np.arange(N_Iter)
-N_x = np.array([25], dtype=int)                    # Number of nodes
-N_y = np.array([25], dtype=int)                   # Number of nodes
+N_x = int(25)                    # Number of nodes
+N_y = int(25)               # Number of nodes
 # omega = np.arange(1, 1.05, 0.05) 
-omega = np.array([1.0])
+omega = np.array([0.5])
 method1 = 'Gauss'
 
 ## Convergence plot
@@ -39,16 +39,36 @@ plt.show()
 """
 
 ## Calculate flux boundary
-u_sol, err_sol = Solver_Iter(Source_block, int(N_x[0]), int(N_y[0]), omega[0], method1, N_Iter) 
+# u_sol, err_sol = Solver_Iter(Source_block, int(N_x[0]), int(N_y[0]), omega[0], method1, N_Iter) 
 # flux_top, flux_bottom, flux_left, flux_right = Flux(u_sol, int(N_x[0]), int(N_y[0]))
 # flux_net = np.concatenate((flux_left, flux_right, flux_bottom, flux_top), axis=0)
 # np.savetxt("flux.csv", flux_net)
 
-u_sol_coarse = I_down(u_sol, N_x, N_y)
+## Testing prolongation and resitrction matrix
+"""
+u_sol_fine = I_up(u_sol, N_x, N_y)
 
 Plotting(u_sol, N_x, N_y)
-Plotting(u_sol_coarse, 13, 13)
+Plotting(u_sol_fine, 49, 49)
 
-np.savetxt('u_sol_fine.csv', u_sol)
-np.savetxt('u_sol_coarse.csv', u_sol_coarse)
+np.savetxt('u_sol_1.csv', u_sol)
+np.savetxt('u_sol_2.csv', u_sol_fine)
+"""
 
+## Multigrid solver
+method = 'Gauss'
+nu_1 = int(1)
+nu_2 = int(1)
+N_factor = int(2)
+
+u_sol, err_sol = MultiGrid(Source_block, N_x, N_y, omega, method, nu_1, nu_2, N_factor, N_Iter)
+
+# Error plot
+fig, ax = plt.subplots()
+cp = ax.plot(N_sol, err_sol, label = ('Omega =' + str(omega)))
+ax.set_yscale('log')
+ax.set_title('Method = ' + method1)
+# ax.set_ybound(lower = 1E-12, upper = 1E+08)
+plt.legend()
+# plt.savefig(fname = (method1 + '_Iter.png'), dpi=600)
+plt.show()
--- Q1b_Sub.py
+++ Q1b_Sub.py
@@ -149,6 +149,8 @@ def Solver_Iter(Source_block, N_x, N_y, omega, method, N_iter):
     cbar = fig.colorbar(cp)
     plt.show()
     """
+
+
     return u_sol, err_sol
 
 def Plotting(u_sol, N_x, N_y):
@@ -167,29 +169,96 @@ def Plotting(u_sol, N_x, N_y):
 
     return 
 
-def TwoGrid(Source_block, N_x, N_y, omega, method, N_iter): 
+'''
+function uh = MG_rec( uh, fh, nu1, nu2, omega, Nmin)
+    uh = relax( uh, fh, nu1, omega);
 
-    return  # u_sol, err_sol
+    if length(uh) > Nmin
+        rh = residual(uh, fh);
+        r2h = restrict(rh);
+        e2h = MG_rec( 0*r2h, r2h, nu1, nu2, omega, Nmin);
+        eh = prolongate(e2h);
+        uh = uh + eh;
+    end
+    
+    uh = relax( uh, fh, nu2, omega);
+end
+'''
+
+def MultiGrid(Source_block, N_x, N_y, omega, method, nu_1, nu_2, N_factor, N_iter):
+    N_temp = N_x
+    N_x = int(N_x)
+    N_y = int(N_y)
+
+    for i1 in range(N_factor):
+        N_temp = (N_temp + 1) / 2
+        N_min = int(N_temp)
+    
+    NumNodes = N_x * N_y;   
+    u_sol = np.zeros((NumNodes,1), dtype=float)   # Vector of f (nx x ny, 1)
+    err_sol = np.zeros(N_iter, dtype=float)
+    
+    for t1 in range(N_iter):
+        u_sol = MultiGrid_Sub(u_sol, Source_block, N_x, N_y, omega, method, nu_1, nu_2, N_min)
+        res_sol = Solver_Relax(u_sol, Source_block, N_x, N_y, omega, method, N_iter=1, mode='Residual')
+        err_sol[t1] = linalg.norm(res_sol, ord=2) 
+        # print(t1)
+
+    return u_sol, err_sol
+
+def MultiGrid_Sub(u_sol, Source_block, N_x, N_y, omega, method, nu_1, nu_2, N_min): 
+    # print('Inception')
+    u_sol = Solver_Relax(u_sol, Source_block, N_x, N_y, omega, method, N_iter=nu_1)      # Relax by nu 1 times
+
+    if N_x > N_min: 
+        res_h = Solver_Relax(u_sol, Source_block, N_x, N_y, omega, method, N_iter=1, mode='Residual')   # Calculate residual
+        print(linalg.norm(res_h, ord=2))
+        N_x_coarse = int((N_x + 1) / 2)     # N_x of coarse grid
+        N_y_coarse = int((N_y + 1) / 2)     # N_y of coarse grid
+        res_2h = I_down(res_h, N_x, N_y, skip_factor=int(2)) 
+        # print("Pre-Inception")
+        # print(res_h.shape)
+        # print(res_2h.shape, N_x_coarse)
+        err_2h = MultiGrid_Sub(0 * res_2h, Source_block, N_x_coarse, N_y_coarse, omega, method, nu_1, nu_2, N_min)
+        err_h = I_up(err_2h, N_x_coarse, N_y_coarse, skip_factor=int(2))
+        u_sol = u_sol + err_h
+
+    u_sol = Solver_Relax(u_sol, Source_block, N_x, N_y, omega, method, N_iter=nu_2)      # Relax by nu 2 times
+
+    return u_sol
 
 def I_up(u_sol, N_x, N_y, skip_factor=int(2)): 
+    # N_x, N_y are dimensions of u_sol (coarse)
     # u_sol on skip_factor * h, u_sol_fine on h
-    N_x = int(skip_factor * N_x)        # dimension of fine mesh
-    N_y = int(skip_factor * N_y)        # dimension of fine mesh                  
+    N_x = int(N_x)        # dimension of coarse mesh
+    N_y = int(N_y)        # dimension of coarse mesh                  
     NumNodes = N_x * N_y;   
     Node = np.arange(0, NumNodes, 1, dtype=float)
     Node = Node.reshape((N_x, N_y)).T
 
     u_sol_fine = []
 
-    for k1 in range(0, N_x, skip_factor):
-        for k2 in range(0, N_y, skip_factor):
-            ANode_i = Node[k2,k1];                       # Setting A_Matrix position for node i,j   
-            ANode_i = int(ANode_i)
-            u_sol_fine.append(u_sol[ANode_i])
-    u_sol_coarse = np.array(u_sol_fine)
-    return 0
+    for k1 in range(0, skip_factor * N_x - 1, 1):
+        for k2 in range(0, skip_factor * N_y - 1, 1):
+            if np.mod(k1, 2) == 0:
+                if np.mod(k2, 2) == 0:
+                    ANode_i = Node[int(k2/2), int(k1/2)];                       # Setting A_Matrix position for node i,j   
+                    ANode_i = int(ANode_i)
+                    u_sol_fine.append(u_sol[ANode_i])  # h, {2i, 2j} = 2h, {i,j}
+                else: 
+                    u_sol_fine.append((0.5 * u_sol[int(Node[ int((k2-1)/2), int(k1/2)] )]) + (0.5 * u_sol[int(Node[ int((k2+1)/2), int(k1/2)] )])) # h, {2i, 2j+1} 
+            else:
+                if np.mod(k2, 2) == 0:
+                    u_sol_fine.append((0.5 * u_sol[int(Node[ int(k2/2), int((k1-1)/2)]) ]) + (0.5 * u_sol[int(Node[ int(k2/2), int((k1+1)/2) ])])) # h, {2i+1, 2j} 
+                else:
+                    u_sol_fine.append((0.25 * u_sol[int(Node[ int(k2/2), int(k1/2)]) ]) + (0.25 * u_sol[int(Node[ int(k2/2), int((k1+1)/2)]) ]) + (0.25 * u_sol[int(Node[ int((k2+1)/2), int(k1/2)]) ]) + (0.25 * u_sol[int(Node[ int((k2+1)/2), int((k1+1)/2)])] )) # h, {2i+1, 2j+1}
+
+    u_sol_fine = np.array(u_sol_fine)
+    
+    return u_sol_fine
 
 def I_down(u_sol, N_x, N_y, skip_factor=int(2)):
+    # N_x, N_y are dimensions of u_sol (fine)
     # u_sol on h, u_sol_coarse on skip_factor * h
     N_x = int(N_x)
     N_y = int(N_y)                
@@ -207,6 +276,119 @@ def I_down(u_sol, N_x, N_y, skip_factor=int(2)):
     u_sol_coarse = np.array(u_sol_coarse)
 
     return u_sol_coarse
+
+def Solver_Relax(u_sol_initial, Source_block, N_x, N_y, omega, method, N_iter, mode='Relax'): 
+    d_x  = 1.0/(N_x-1);         # delta x                     
+    d_y = 1.0/(N_y-1);          # delta y                     
+    NumNodes = N_x * N_y;   
+    NumNodes_interior = int((N_x - 1) / 6) - 1    # Number of interior nodes per block 
+
+    # Initializing sparse matrices 'A' and 'Node' (coefficient value at i,j)
+    A = sparse.lil_matrix((NumNodes, NumNodes), dtype=np.double)
+    Node = np.arange(0, NumNodes, 1, dtype=float)
+    Node = Node.reshape((N_x, N_y)).T
+    RHS = np.zeros((NumNodes,1), dtype=float)       # Vector of f (nx x ny, 1)
+
+    # Interior points of matrix A 
+    for i1 in range(1,N_x-1):
+        for i2 in range(1,N_y-1):
+            # Point (x, y) = (i1, i2)
+            # Iterate through all interior points
+            ANode_i = Node[i1,i2];                       # Setting A_Matrix position for node i,j   
+            ANode_i = int(ANode_i)
+            x_local = i1 * d_x
+            y_local = i2 * d_y
+
+            # Insert coefficients for the finite difference approximation
+            A[ANode_i, int(Node[i1-1, i2])] = - 1.0 / (d_x**2.0)
+            A[ANode_i, int(Node[i1, i2-1])] = - 1.0 / (d_y**2.0)
+            A[ANode_i, int(Node[i1, i2])] = (2.0 / (d_x**2.0)) + (2.0 / (d_y**2.0))
+            A[ANode_i, int(Node[i1, i2+1])] = - 1.0 / (d_y**2.0)
+            A[ANode_i, int(Node[i1+1, i2])] = - 1.0 / (d_x**2.0) 
+
+    # Bottom domain B'C'
+    for j1 in range(1,N_x):
+        j2 = 0                           # First row at the bottom
+        ANode_i = int(Node[j1,j2])       # Setting A_Matrix position for node i,j   
+        A[ANode_i, int(Node[j1,j2])] = 1/(d_x**2.0)# 1.0 
+
+    # Top domain A'D'
+    for j1 in range(1,N_x-1):    
+        j2 = N_y - 1
+        ANode_i = int(Node[j1,j2])       # Setting A_Matrix position for node i,j  
+        A[ANode_i, int(Node[j1,j2])] = 1/(d_x**2.0) # 1.0 
+
+    # Left domain A'B'
+    for j2 in range(1,N_y-1):    
+        j1 = 0
+        ANode_i = int(Node[j1,j2])       # Setting A_Matrix position for node i,j  
+        A[ANode_i, int(Node[j1,j2])] = 1/(d_x**2.0) # 1.0 
+
+    # Right domain C'D'
+    for j2 in range(1,N_y-1):
+        j1 = N_x - 1
+        ANode_i = int(Node[j1,j2])       # Setting A_Matrix position for node i,j   
+        A[ANode_i, int(Node[j1,j2])] = 1/(d_x**2.0) # 1.0
+
+    # Bottom left corner point
+    ANode_i = int(Node[0,0])
+    A[ANode_i, int(Node[0,0])] = 1/(d_x**2.0) # 1.0
+
+    # Bottom right corner point
+    ANode_i = int(Node[N_x-1,0])
+    A[ANode_i, int(Node[N_x-1,0])] = 1/(d_x**2.0) # 1.0
+
+    # Top left corner point
+    ANode_i = int(Node[0, N_y-1])
+    A[ANode_i, int(Node[0,N_y-1])] = 1/(d_x**2.0) # 1.0
+
+    # Top right corner point
+    ANode_i = int(Node[N_x-1,N_y-1])
+    A[ANode_i, int(Node[N_x-1,N_y-1])] = 1/(d_x**2.0) # 1.0
+
+    # Vector RHS 
+    column_block = np.mod(Source_block - 1, 4) + 1          # Column index of source blocks
+    row_block = np.floor_divide(Source_block - 1, 4) + 1    # Row index of source blocks
+
+    for s1 in range(Source_block.shape[0]):
+        btm_left_x = row_block[s1] * (NumNodes_interior + 1)
+        btm_left_y = column_block[s1] * (NumNodes_interior + 1)
+        top_right_x = btm_left_x + NumNodes_interior + 1
+        top_right_y = btm_left_y + NumNodes_interior + 1
+
+        for i1 in range(btm_left_x, top_right_x+1):
+            for i2 in range(btm_left_y, top_right_y+1):
+                # Point (x, y) = (i1, i2)
+                # Iterate through all interior points
+                ANode_i = Node[i1,i2];                       # Setting A_Matrix position for node i,j   
+                ANode_i = int(ANode_i)
+                # Insert coefficients for the RHS vector
+                RHS[ANode_i] = 1.0   # Equation on RHS
+
+    u_sol = u_sol_initial  # Vector of f (nx x ny, 1)
+
+    if mode == "Residual":
+        return (RHS - A @ u_sol)
+
+    D = np.diag(np.diag(A.toarray()))
+    L = np.tril(-A.toarray(), k=-1)
+    U = np.triu(-A.toarray(), k=1)
+    I = np.identity(NumNodes, dtype=float)
+
+    if method == 'Jacobi':
+        D_inv = linalg.inv(D)
+        R = I - (D_inv @ A)    
+        f = D_inv @ RHS
+    elif method == 'Gauss':
+        D_L_inv = linalg.inv(D - L)
+        R = D_L_inv @ U   
+        f = D_L_inv @ RHS
+
+    for t1 in range(N_iter):
+        u_sol = (omega * ((R @ u_sol) + f)) + ((1.0 - omega) * u_sol)
+
+    return u_sol
+
 
 def Flux(u_sol, N_x, N_y): 
     d_x  = 1.0/(N_x-1);         # delta x                     
